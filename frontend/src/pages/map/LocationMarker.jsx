6344ed67f2b8674668885c4af905648e1aa10f93
Nils <83452014+Sneezy123@users.noreply.github.com>
2025-05-04 22:10:48 +0200

    feat: add custom and user position markers for map integration

--- frontend/src/pages/map/LocationMarker.jsx
+++ frontend/src/pages/map/LocationMarker.jsx
@@ -11,12 +11,8 @@ function CustomMarker() {
             <circle cx='100' cy='100' r='100'></circle>
         </svg> */
 
-        <span
-            className={`relative flex size-3`}
-            title='Diese Veranstaltung läuft aktuell.'
-        >
-            <span className='absolute inline-flex h-full w-full rounded-full bg-sky-500 opacity-50'></span>
-            <span className='relative inline-flex size-3 rounded-full bg-sky-500'></span>
+        <span className={`relative flex size-2.5`}>
+            <span className='relative inline-flex size-2.5 rounded-full bg-white border-2 border-black cursor-pointer'></span>
         </span>
     );
 }
